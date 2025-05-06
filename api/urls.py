bb08c4d3cfb7b6af7d919ad231dbc42e1e7bda61
minhlc1108 <minh.lcm.2003@gmail.com>
2025-05-06 05:52:03 +0000

    auth

--- api/urls.py
+++ api/urls.py
@@ -11,8 +11,8 @@ from api.views import (
     LoginView,
     RegisterView,
     LogoutView,
+    CookieTokenRefreshView,
 )
-from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("artists/", ArtistListView.as_view(), name="artist-list"),
@@ -26,5 +26,5 @@ urlpatterns = [
     path("auth/register/", RegisterView.as_view(), name="register"),
     path("auth/login/", LoginView.as_view(), name="login"),
     path("auth/logout/", LogoutView.as_view(), name="logout"),
-    path("auth/refreshToken/", TokenRefreshView.as_view(), name="refreshToken"),
+    path("auth/refreshToken/", CookieTokenRefreshView.as_view(), name="refreshToken"),
 ]

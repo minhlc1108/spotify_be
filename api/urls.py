8b3be58ee12f2317315f1690374a41765f8002c4
huyanhtat <127735540+huyanhtat@users.noreply.github.com>
2025-05-11 22:48:19 +0700

    Update urls.py

--- api/urls.py
+++ api/urls.py
@@ -13,6 +13,45 @@ from api.views import (
     LogoutView,
     PlayStateView,
     CookieTokenRefreshView,
+    PlaylistListView,
+    PlaylistDetailView
+    SearchView,
+)
+
+urlpatterns = [
+    path("artists/", ArtistListView.as_view(), name="artist-list"),
+    path("artists/<uuid:pk>/", ArtistDetailView.as_view(), name="artist-detail"),
+    path("genres/", GenreListView.as_view(), name="artist-list"),
+    path("genres/<uuid:pk>/", GenreDetailView.as_view(), name="artist-detail"),
+    path("albums/", AlbumListView.as_view(), name="artist-list"),
+    path("albums/<uuid:pk>/", AlbumDetailView.as_view(), name="artist-detail"),
+    path("tracks/", TrackListView.as_view(), name="artist-list"),
+    path("tracks/<uuid:pk>/", TrackDetailView.as_view(), name="artist-detail"),
+    path("search/", SearchView.as_view(), name="search"),
+    path("auth/register/", RegisterView.as_view(), name="register"),
+    path("auth/login/", LoginView.as_view(), name="login"),
+    path("auth/logout/", LogoutView.as_view(), name="logout"),
+    path("playstate/", PlayStateView.as_view(), name="playState"),
+    path("auth/refreshToken/", CookieTokenRefreshView.as_view(), name="refreshToken"),
+    path("playlists/", PlaylistListView.as_view(), name="playlist-list"),
+    path("playlists/<uuid:pk>/", PlaylistDetailView.as_view(), name="playlist-detail"),
+]
+
+from django.urls import path
+from api.views import (
+    ArtistListView,
+    ArtistDetailView,
+    GenreListView,
+    GenreDetailView,
+    AlbumListView,
+    AlbumDetailView,
+    TrackListView,
+    TrackDetailView,
+    LoginView,
+    RegisterView,
+    LogoutView,
+    PlayStateView,
+    CookieTokenRefreshView,
     SearchView,
     PlaylistListView,
     PlaylistDetailView,

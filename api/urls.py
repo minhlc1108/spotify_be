6384001c4f179621efb0c7eec2e6bff906a647ca
minhlc1108 <minh.lcm.2003@gmail.com>
2025-05-12 07:04:49 +0000

    merge

--- api/urls.py
+++ api/urls.py
@@ -17,14 +17,6 @@ from api.views import (
 )
 
 urlpatterns = [
-    path("artists/", ArtistListView.as_view(), name="artist-list"),
-    path("artists/<uuid:pk>/", ArtistDetailView.as_view(), name="artist-detail"),
-    path("genres/", GenreListView.as_view(), name="artist-list"),
-    path("genres/<uuid:pk>/", GenreDetailView.as_view(), name="artist-detail"),
-    path("albums/", AlbumListView.as_view(), name="artist-list"),
-    path("albums/<uuid:pk>/", AlbumDetailView.as_view(), name="artist-detail"),
-    path("tracks/", TrackListView.as_view(), name="artist-list"),
-    path("tracks/<uuid:pk>/", TrackDetailView.as_view(), name="artist-detail"),
     path("search/", SearchView.as_view(), name="search"),
     path("artist/", ArtistListView.as_view(), name="artist-list"),
     path("artist/<uuid:pk>/", ArtistDetailView.as_view(), name="artist-detail"),
